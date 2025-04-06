6a6af0a12946c7b3d306c7b5cfa22d1d1420d9b3
kushkapadia <kush.salesbrahma@gmail.com>
2025-04-06 09:32:37 +0530

    Whats app notification implemented

--- whatsapp server/routes/router.js
+++ whatsapp server/routes/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const whatsappNotification = require('../helper/whatsappNotification');
+const userCollection = require("../db").db().collection("users");
+const WhatsappNotification = require('../helper/whatsappNotification');
 //imports here
 const userRoutes = require("./userRoutes");
 
@@ -10,13 +11,13 @@ router.get("/health-check", (req,res)=>{
   res.json("Server Health: OK");
 })
 
-router.post("/request-access",(req,res)=>{
+router.post("/request-access",async (req,res)=>{
   const { email } = req.body;
   console.log(email);
   //write mongodb query to find user with email
-   let userDoc = userCollection.findOne({email: email});
-  
-   whatsappNotification = new whatsappNotification(userDoc.adminMobile, `Hello! Request Access to a document is sent by ${email} of ${userDoc.department} department and role:${role}. Please check the document and approve it.`);
+   let userDoc = await userCollection.findOne({email: email});
+  console.log(userDoc);
+  let whatsappNotification = new WhatsappNotification(userDoc.admin_mobile, `Hello! Request Access to a document is sent by ${email} of ${userDoc.department} department and role:${userDoc.role}. Please check the document and approve it.\nDo you want to approve?`);
     whatsappNotification.sendWhatsappNotification();
   // Perform any necessary operations with the phone number
   // For example, you can save it to a database or send a message
